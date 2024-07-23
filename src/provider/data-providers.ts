5385abe87e81e9278a539edbb61ad1b108c55005
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-07-23 23:40:26 +0300

    feat: configure data providers interface

---
+++ src/provider/data-providers.ts
@@ -0,0 +1,72 @@
+import { DataProvider } from "react-admin";
+import { HarenaProviders } from "./types";
+
+export const getProvider = (resource: string): HarenaProviders<any> => {
+  switch (resource) {
+    case "patrimoine":
+
+    default:
+      throw new Error("Unknown resource type");
+  }
+};
+
+export const dataProvider: DataProvider = {
+  create: async (resource, { meta, data: payload }) => {
+    return {
+      data: await getProvider(resource).saveOrUpdate(payload, {
+        ...meta,
+        mutationType: "CREATE",
+      }),
+    };
+  },
+  update: async (resource, { data: payload, meta }) => {
+    return {
+      data: await getProvider(resource).saveOrUpdate(payload, {
+        ...meta,
+        mutationType: "UPDATE",
+      }),
+    };
+  },
+  getList: async (
+    resource,
+    { pagination = { page: 1, perPage: 10 }, sort, filter, meta }
+  ) => {
+    const response = await getProvider(resource).getList(
+      pagination.page,
+      pagination.perPage,
+      filter,
+      sort,
+      meta
+    );
+    return {
+      data: response,
+      total: response.length,
+      pageInfo: {
+        hasNextPage: response.length >= pagination.perPage,
+        hasPreviousPage: pagination.page > 1,
+      },
+    };
+  },
+  getOne: async (resource, { id: payloadId, meta }) => {
+    return {
+      data: await getProvider(resource).getOne(payloadId as string, meta),
+    };
+  },
+  delete: async (resource, { id: payloadId, meta }) => {
+    return {
+      data: await getProvider(resource).delete(payloadId as string, meta),
+    };
+  },
+  getMany: () => {
+    throw new Error("Not Implemented");
+  },
+  getManyReference: () => {
+    throw new Error("Not Implemented");
+  },
+  updateMany: () => {
+    throw new Error("Not Implemented");
+  },
+  deleteMany: () => {
+    throw new Error("Not Implemented");
+  },
+};
