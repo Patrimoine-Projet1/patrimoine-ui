1f5f88f6f81e753fb4cfa08894c576bb4de9f292
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-07-27 22:15:19 +0300

    feat: implement patrimoine show

--- src/operation/patrimoine/index.tsx
+++ src/operation/patrimoine/index.tsx
@@ -1,6 +1,8 @@
 import patrimoineList from "./patrimoineList";
+import patrimoineShow from "./patrimoineShow";
 
 const patrimoines = {
   list: patrimoineList,
+  show: patrimoineShow,
 };
 export default patrimoines;
---
+++ src/operation/patrimoine/patrimoineShow.tsx
@@ -0,0 +1,45 @@
+import { Box } from "@mui/material";
+import { useParams } from "react-router-dom";
+import {
+  EditButton,
+  FunctionField,
+  Show,
+  SimpleShowLayout,
+  TextField,
+  Toolbar,
+} from "react-admin";
+
+import { Money } from "../../utils/money";
+import { HaDateField } from "../../utils/date";
+
+const ActionShow = () => {
+  return (
+    <Toolbar>
+      <EditButton />
+    </Toolbar>
+  );
+};
+
+export default function patrimoineShow() {
+  const { id } = useParams();
+  return (
+    <Box>
+      <Show id={id} actions={<ActionShow />}>
+        <SimpleShowLayout>
+          <TextField source="nom" label="nom" />
+          <FunctionField
+            label="Date"
+            render={(patrimoine) => {
+              return <HaDateField value={patrimoine?.t} />;
+            }}
+          />
+          <TextField source="possesseur.nom" label="possesseur.nom" />
+          <FunctionField
+            render={(patrimoine) => Money(patrimoine.valeur_comptable)}
+            label="Valeur Comptable"
+          />
+        </SimpleShowLayout>
+      </Show>
+    </Box>
+  );
+}
